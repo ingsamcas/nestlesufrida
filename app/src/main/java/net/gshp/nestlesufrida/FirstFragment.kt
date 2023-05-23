fef0e6d5200df8bca4eecc5bdf9893fd8a423a10
Hugo Flores <hugofloresbravo@gmail.com>
2023-05-23 01:22:46 -0600

    login xml

--- app/src/main/java/net/gshp/nestlesufrida/FirstFragment.kt
+++ app/src/main/java/net/gshp/nestlesufrida/FirstFragment.kt
@@ -1,11 +1,13 @@
 package net.gshp.nestlesufrida
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import net.gshp.nestlesufrida.databinding.FragmentFirstBinding
 
 /**
@@ -19,6 +21,7 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,11 +34,40 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUI();
+        initListeners(view)
+    }
 
-        binding.buttonFirst.setOnClickListener {
+    private fun initUI() {
+        val version ="v. ${BuildConfig.VERSION_NAME}"
+        binding.tvVersion.text = version
+    }
+
+    private fun initListeners(view: View){
+        binding.tvTerms.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        binding.buttonLogin.setOnClickListener(){
+            Snackbar.make(view, "esto es una prueba", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        binding.tvSupport.setOnClickListener(){
+            //findNavController().navigate(R.id.action_FirstFragment_to_SupportFragment)
+            showDialog()
+        }
+
     }
+
+    private fun showDialog(){
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.dialog_support)
+
+        dialog.show()
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
